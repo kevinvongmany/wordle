3fcead9d05c75e774f474b16e093959d34812230
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-25 19:54:36 +1100

    Updated footer to be white text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ export default function Footer() {
         <footer className="footer-bg fixed w-full z-20 bottom-0 start-0 border-t border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-black dark:text-gray-400">
                 <div className='flex space-x-4'>
-                    <a href="https://github.com/kevinvongmany" target="_blank" rel="noopener noreferrer" className="text-black dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200">
+                    <a href="https://github.com/kevinvongmany" target="_blank" rel="noopener noreferrer" className="text-white dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200">
                         <FaGithub className="w-6 h-6" />
                     </a>
                 </div>
-                <p>© 2024 Wilfred Owen</p>
+                <p className='text-white'>© 2024 Wilfred Owen</p>
             </div>
         </footer>
     );
