c1af54c3dc9b3b265ce10021a2ff796cdc3d4e49
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-30 16:34:18 +1100

    Added base word list and credits

--- src/Words.jsx
+++ src/Words.jsx
@@ -1,4 +1,7 @@
-import wordBank from './wordle-words.json';
+import wordsFour from './wordlist-4.json';
+import wordsFive from './wordlist-5.json';
+import wordsSix from './wordlist-6.json';
+import wordsSeven from './wordlist-7.json';
 export const boardDefault = [
     ['','','','',''],
     ['','','','',''],
@@ -9,7 +12,7 @@ export const boardDefault = [
 ];
 
 export const generateWordSet = async () => {
-    const wordSet = new Set(wordBank);
+    const wordSet = new Set(wordsFive);
 
     return {wordSet};
 };
