bd644d26154773020ebf299bf49cde1c27ead5de
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-01 09:28:56 +1100

    Added credits to artist

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -9,13 +9,15 @@ function GameOver() {
         <h3 className="text-xl md:text-3xl mb-4 md:mb-6">{gameOver.guessedWord ? "You guessed the word correctly!" : "Better luck next time!"}</h3>
         <h1 className="text-2xl md:text-5xl mb-4 md:mb-6">The word was: {targetWord}</h1>
         {gameOver.guessedWord && (
-            <h2 className="text-xl md:text-3xl">It took you {currentAttempt.attempt} attempt{currentAttempt.attempt > 1 && "s"} to guess the word</h2>
+            <h2 className="text-xl md:text-3xl">It took you {currentAttempt.attempt} attempt{currentAttempt.attempt > 1 && "s"} to guess the word </h2> 
         )}
         {/* CUSTOM MESSAGE  */}
+          <p className="text-xl md:text-3xl mt-3 font-bold">The emotes you see on page are from the great work of <a className="text-blue-600 underline" href='https://twitch.tv/mang0luck'>Mang0Luck</a>!</p>
+          <p className="text-xl md:text-3xl mt-3 font-bold">Please check out their work at <a className="text-blue-600 underline" href='https://lam4ngo.carrd.co/'>https://lam4ngo.carrd.co/</a>!</p>
         <div className="mt-6 md:mt-12 flex items-center justify-center">
           <img src={BogJam} alt="Bog Jam" className="w-14 md:w-32 items-center flex" />
         </div>
-          <p className="text-xl md:text-3xl mt-3">Want more Wordle? Check out my <a className="text-blue-600 underline" href='https://kevdle.netlify.app'>unthemed version</a>!</p>
+          <p className="text-xl md:text-xl mt-3">Want more Wordle? Check out my <a className="text-blue-600 underline" href='https://kevdle.netlify.app'>unthemed version</a>!</p>
         {/*  */}
     </div>
   )
