ed643a20d141da475c5bb8932442b483cb97d67f
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-01 09:44:45 +1100

    Updated credit size as per artist request

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -12,12 +12,11 @@ function GameOver() {
             <h2 className="text-xl md:text-3xl">It took you {currentAttempt.attempt} attempt{currentAttempt.attempt > 1 && "s"} to guess the word </h2> 
         )}
         {/* CUSTOM MESSAGE  */}
-          <p className="text-xl md:text-3xl mt-3 font-bold">The emotes you see on page are from the great work of <a className="text-blue-600 underline" href='https://twitch.tv/mang0luck'>Mang0Luck</a>!</p>
-          <p className="text-xl md:text-3xl mt-3 font-bold">Please check out their work at <a className="text-blue-600 underline" href='https://lam4ngo.carrd.co/'>https://lam4ngo.carrd.co/</a>!</p>
         <div className="mt-6 md:mt-12 flex items-center justify-center">
           <img src={BogJam} alt="Bog Jam" className="w-14 md:w-32 items-center flex" />
         </div>
-          <p className="text-xl md:text-xl mt-3">Want more Wordle? Check out my <a className="text-blue-600 underline" href='https://kevdle.netlify.app'>unthemed version</a>!</p>
+          <p className="text-xl md:text-xl mt-3">Want more Wordle? Check out the <a className="text-blue-600 underline" href='https://kevdle.netlify.app'>unthemed version</a>!</p>
+          <p className="text-md md:text-xl mt-3">This emote is from the great work of Mang0Luck, feel free to check out more work on their Twitter/X: <a className="text-blue-600 underline" href='https://x.com/lam4ngo?lang=en'>@lam4ngo</a></p>
         {/*  */}
     </div>
   )
