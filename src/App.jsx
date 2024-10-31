70bb1a68a46f0da6889dc37a98f22867994f5e3f
Kevin Vongmany <kevin.vongmany@techmedia.com.au>
2024-10-31 17:07:35 +1100

    updated target word

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import GameOver from "./components/GameOver";
 import Footer from "./components/Footer";
 function App() {
   
-  const targetWord = "ENABLER";
+  const targetWord = "almonds";
   const letterCol = targetWord.length;
   const wordRows = letterCol < 6 ? 6 : letterCol + 1;
 
