c8d9911c7ec3dc88f3ae3f5bdf4e127b5d6f2300
Kevin Vongmany <93503441+kevinvongmany@users.noreply.github.com>
2024-11-01 19:52:14 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import GameOver from "./components/GameOver";
 import Footer from "./components/Footer";
 function App() {
   
-  let targetWord = "AUDIO";
+  let targetWord = "STEEL";
   targetWord = targetWord.toUpperCase();
   const letterCol = targetWord.length;
   const wordRows = letterCol < 6 ? 6 : letterCol + 1;
