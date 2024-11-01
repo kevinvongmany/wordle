bb1cbd2f0bbc6dbc9bc9341bc4f26dd9618cf8aa
Kevin Vongmany <kevin.vongmany@techmedia.com.au>
2024-11-01 16:29:05 +1100

    Updated target word to be a 5 etter vowel intensive word

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import GameOver from "./components/GameOver";
 import Footer from "./components/Footer";
 function App() {
   
-  let targetWord = "STARCH";
+  let targetWord = "AUDIO";
   targetWord = targetWord.toUpperCase();
   const letterCol = targetWord.length;
   const wordRows = letterCol < 6 ? 6 : letterCol + 1;
