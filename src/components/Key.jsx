495c5a74fa8b42b48f85f727d4e5d8b169678b1b
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-03 12:49:49 +1100

    Added keyboard component

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -3,24 +3,16 @@ import { useContext } from "react";
 import AppContext from "../AppContext";
 import { FaBackspace } from "react-icons/fa";
 
-function Key({ keyValue, isLarge, used }) {
-  const { onSelectLetter, onDelete, onEnter, targetWord, solutionArray } = useContext(AppContext);
-  const almost = solutionArray.includes(keyValue) && used;
-  const correct = targetWord.includes(keyValue) && !almost && used;
-  const selectletter = () => {
-    if (keyValue === "DEL") {
-      onDelete();
-    } else if (keyValue === "ENTER") {
-      onEnter();
-    } else {
-      onSelectLetter(keyValue);
-    }
-  };
+function Key({ keyValue, colour, isLarge, used }) {
+  const { handleKeyUp } = useContext(AppContext);
+  console.log(keyValue);
+  // console.log(handleKeyUp);
+
   return (
     <div 
-    className={`w-9 h-12 md:w-24 md:h-24 m-0.5 md:m-1.5 rounded grid place-items-center text-sm md:text-2xl bg-gray-600 text-white font-sans cursor-pointer ${almost ? 'almost' : correct ? 'correct' : ''}`}
-    id={isLarge ? "large" : used && "disabled"} 
-    onClick={selectletter}>
+      className={`w-9 h-12 md:w-24 md:h-24 m-0.5 md:m-1.5 rounded grid place-items-center text-sm md:text-2xl bg-gray-600 text-white font-sans cursor-pointer ${colour}`}
+      id={isLarge ? "large" : used && "disabled"} 
+      onClick={() => handleKeyUp(keyValue)}>
       {keyValue === "DEL" ? <FaBackspace /> : keyValue}
     </div>
   );
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -6,40 +6,45 @@ function Keyboard() {
   const keys1 = 'QWERTYUIOP'.split('');
   const keys2 = 'ASDFGHJKL'.split('');
   const keys3 = 'ZXCVBNM'.split('');
-  const { onSelectLetter, onDelete, onEnter, usedLetters } = useContext(AppContext);
-  const handleKeyboard = useCallback((e) => {
-    const key = e.key.toUpperCase();
-    if (key === 'ENTER') {
-      onEnter();
-    } else if (key === 'BACKSPACE') {
-      onDelete();
-    } else if (/^[A-Z]$/.test(key)) {
-      onSelectLetter(key);
-    }
-  }, [onDelete, onSelectLetter, onEnter]);
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyboard);
-    return () => {
-      document.removeEventListener('keydown', handleKeyboard);
-    }
-  }, [handleKeyboard])
+  const { usedLetters, usedKeys } = useContext(AppContext);
+
+
+  // const handleKeyboard = useCallback((e) => {
+  //   const key = e.key.toUpperCase();
+  //   if (key === 'ENTER') {
+  //     onEnter();
+  //   } else if (key === 'BACKSPACE') {
+  //     onDelete();
+  //   } else if (/^[A-Z]$/.test(key)) {
+  //     onSelectLetter(key);
+  //   }
+  // }, [onDelete, onSelectLetter, onEnter]);
+  // useEffect(() => {
+  //   document.addEventListener('keydown', handleKeyboard);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleKeyboard);
+  //   }
+  // }, [handleKeyboard])
   return (
-    <div className='mt-6 md:mt-12 lg:mt-12 w-full h-full' onKeyDown={handleKeyboard}>
+    <div className='mt-6 md:mt-12 lg:mt-12 w-full h-full'>
         <div className='flex row justify-center items-center mb-1'>
-            {keys1.map((key, i) => (
-                <Key key={i} keyValue={key} used={usedLetters.includes(key)} />
-            ))}
+            {keys1.map((key, i) => {
+                const colour = usedKeys[key];
+                return <Key key={i} colour={colour} keyValue={key} />
+            })}
         </div>
         <div className='flex row justify-center items-center mb-1'>
-            {keys2.map((key, i) => (
-                <Key key={i} keyValue={key} used={usedLetters.includes(key)} />
-            ))}
+            {keys2.map((key, i) => {
+                const colour = usedKeys[key];
+                return <Key key={i} colour={colour} keyValue={key} />
+            })}
         </div>
         <div className='flex row justify-center items-center mb-1'>
             <Key keyValue={"ENTER"} isLarge />
-            {keys3.map((key, i) => (
-                <Key key={i} keyValue={key} used={usedLetters.includes(key)} />
-            ))}
+            {keys3.map((key, i) => {
+                const colour = usedKeys[key];
+                return <Key key={i} colour={colour} keyValue={key} />
+            })}
             <Key keyValue={"DEL"} isLarge />
 
         </div>
