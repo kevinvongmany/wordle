e965696b05d01b499af8951facfd6fde8f1ff766
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-05 22:49:02 +1100

    Updated footer to link Wordpam

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
           <div className="flex flex-col items-center mb-4 pb-">
             <Board />
             <Keyboard />
-            <p>Want more Wordle? Try my <a href='https://kevdle.netlify.app/' className='text-blue-400 text-underline' target='_blank'>unthemed version</a>!</p>
+            <p>Want more Wordle? Try my <a href='https://kevdle.netlify.app/' className='text-blue-400 text-underline' target='_blank'>unthemed version</a> or <a href='https://wordpam.netlify.app/' className='text-blue-400 text-underline' target='_blank'>Wordpam</a>!</p>
             {showModal && <GameOver />}
 
           </div>
