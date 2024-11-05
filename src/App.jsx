91e0668bf810169bd9ae8a7c87446dd2f169d4cc
Kevin Vongmany <93503441+kevinvongmany@users.noreply.github.com>
2024-11-05 12:01:10 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  let targetWord = "spare";
+  let targetWord = "pelt";
   targetWord = targetWord.toUpperCase();
 
   const {turn, currentGuess, guesses, isCorrect, maxTurns, usedKeys, handleKeyUp} = useWordle(targetWord);
