7d4a0fc97f7988dde40d3ee3590dc2f67aa82ab1
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-06 20:48:09 +1100

    Updated wording of share clipboard

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -33,7 +33,7 @@ const Results = () => {
   }
 
   const handleCopy = () => {
-    const shareString = `Kevdle Results: (${turn}/${maxTurns})\n\n${generateWordleResult(guesses).join("\n")}\n\nhttps://kevdle.netlify.app/`;
+    const shareString = `Kevdle score ${turn}/${maxTurns}\n\n${generateWordleResult(guesses).join("\n")}\n\nhttps://kevdle.netlify.app/`;
     copy(shareString);
     toast.success("Copied to clipboard!");
   }
