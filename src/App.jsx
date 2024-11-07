d64678b3521e7111e3c3ed0e6f9c3647dd5abb60
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-07 20:19:44 +1100

    Updated wordpam

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  let targetWord = "SHXVEL";
+  let targetWord = "BURGER";
   targetWord = targetWord.toUpperCase();
 
   const {turn, currentGuess, guesses, isCorrect, maxTurns, usedKeys, handleKeyUp} = useWordle(targetWord);
