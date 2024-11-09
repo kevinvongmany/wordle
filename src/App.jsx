b5da18aad5ecc13bbbe3d879df3f1b744aa695db
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-10 08:45:51 +1100

    Updated wordpam

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("TAKEN");
+  const [targetWord, setTargetWord] = useState("MELON");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
