d44b2ce792cf088bc382b39b45ed6f1c67384473
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-10 14:17:27 +1100

    Updated front page

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,6 @@ function App() {
         >
           <div className="flex flex-col items-center mb-4 pb-">
             <Board />
-            <p>Seven letters?!?!</p>
             <Keyboard />
             <p>Want more Wordle? Try my <a href='https://kevdle.netlify.app/' className='text-blue-400 text-underline' target='_blank'>unthemed version</a> or <a href='https://wordpam.netlify.app/' className='text-blue-400 text-underline' target='_blank'>Wordpam</a>!</p>
             {showModal && <GameOver />}
