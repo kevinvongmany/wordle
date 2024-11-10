5be44a71e98c76414aacd5c1d340122bcfb43874
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-11 00:15:17 +1100

    Updated word of the day

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("LOLFFY");
+  const [targetWord, setTargetWord] = useState("SORAIN");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
