b95ee6a02f5330c461dc5ec61083847d5f8e225b
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-10 14:16:29 +1100

    Updated word of the day

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("ORBDTWO");
+  const [targetWord, setTargetWord] = useState("LOLFFY");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -21,11 +21,6 @@ function GameOver() {
 
         <h1 className="text-2xl md:text-5xl mb-4 md:mb-6">The word was {targetWord}</h1>
         {/* Custom message */}
-        <p className="text-lg md:text-2xl mb-4 md:mb-6 text-red-600">Subscribe to Orb_D2's YouTube channel</p>
-        <p className="text-xl md:text-5xl mb-4 md:mb-6 text-blue-400"><a href="https://www.youtube.com/@TheSecondOrb." target='_blank'>yt/@TheSecondOrb.</a></p>
-        <p className="text-lg md:text-2xl mb-4 md:mb-6 text-violet-600">Follow him on Twitch!</p>
-        <p className="text-xl md:text-5xl mb-4 md:mb-6 text-blue-400"><a href="https://www.twitch.tv/orb_d2"  target='_blank'>ttv/orb_d2</a></p>
-        
         {/*  */}
 
         <p>Want more Wordle? Try my <a href='https://kevdle.netlify.app/' className='text-blue-400 text-underline' target='_blank'>unthemed version</a> or try Wordpam!</p>
