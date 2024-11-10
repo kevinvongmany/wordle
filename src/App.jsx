00595ecf521aa00611a833ba6b415d69246941a1
Kevin Vongmany <93503441+kevinvongmany@users.noreply.github.com>
2024-11-11 10:30:10 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("CONJURE");
+  const [targetWord, setTargetWord] = useState("PROWL");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
