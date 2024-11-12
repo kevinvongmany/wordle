52fb462303556e3f2c5c0d491176101769737609
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-12 23:34:48 +1100

    update of word for test

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("ORBSUCC");
+  const [targetWord, setTargetWord] = useState("BEPIS");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
