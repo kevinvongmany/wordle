9533f1f9bf4b8fd34423cd11e862ed2d475fdd04
Kevin Vongmany <kevin.vongmany@techmedia.com.au>
2024-11-14 13:06:38 +1100

    Updated bogdle of the day 14 november

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,52 +1,68 @@
-import React, {useContext} from 'react'
-import AppContext from '../AppContext'
-import Results from './Results'
+import React, { useContext } from "react";
+import AppContext from "../AppContext";
+import Results from "./Results";
 import { IoIosCloseCircle } from "react-icons/io";
 
-
 function GameOver() {
-  const {targetWord, turn, isCorrect, setShowModal} = useContext(AppContext)
+  const { targetWord, turn, isCorrect, setShowModal } = useContext(AppContext);
   console.log(isCorrect);
 
   const handleCloseModal = () => {
     setShowModal(false);
-  }
-
+  };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out">
-      <div className="bg-black p-12 rounded-3xl shadow-lg text-center relative transform transition-transform duration-300 ease-in-out translate-y-[-50px] opacity-100 animate-fade-in">
-        <button 
-          className="absolute top-4 right-4 text-white text-2xl" 
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-black p-12 rounded-3xl shadow-lg text-center relative">
+        <button
+          className="absolute top-4 right-4 text-white text-2xl"
           onClick={handleCloseModal}
         >
-          <p className='text-5xl text-red-500 hover:text-red-400'><IoIosCloseCircle /></p>
+          <p className="text-5xl text-red-500 hover:text-red-400">
+            <IoIosCloseCircle />
+          </p>
         </button>
-        <h3 className="text-xl md:text-3xl mb-4 md:mb-6">{isCorrect ? "You guessed the word correctly!" : "Better luck next time!"}</h3>
+        <h3 className="text-xl md:text-3xl mb-4 md:mb-6">
+          {isCorrect ? "Nice work!" : "You just got bogged up!"}
+        </h3>
         {isCorrect && (
-          <h2 className="text-xl md:text-3xl mb-6">You had {turn} attempt{turn > 1 && "s"} to guess the word!</h2>
+          <h2 className="text-xl md:text-3xl mb-6">
+            You guessed the word in {turn} attempt{turn > 1 && "s"}!
+          </h2>
         )}
         <Results />
 
         {/* Custom message */}
+        <p className="text-xl">
+          Relive this moment by watching Bog defeat{" "}
+          <a
+            className="text-blue-500 underline font-bold"
+            href="https://youtu.be/UYSF864zuQs?t=4146"
+            target="_blank"
+          >
+            10 raid bosses back-to-back without a single wipe
+          </a>
+          !
+        </p>
         {/*  */}
-        <h1 className="text-2xl md:text-5xl my-4 md:my-6">The word was {targetWord}</h1>
+        <h1 className="text-2xl md:text-5xl my-4 md:my-6">
+          The word was {targetWord}
+        </h1>
 
-        <div className='flex justify-center space-x-4'>
-          <a href='https://wordpam.com' target='_blank'>
+        <div className="flex justify-center space-x-4">
+          <a href="https://wordpam.com" target="_blank">
             <button className="bg-gray-800 text-white px-6 py-3 mt-8 rounded text-4xl hover:bg-gray-700 transition duration-300">
               Play Wordpam
             </button>
           </a>
-          <a href='https://kevdle.netlify.app/' target='_blank'>
+          <a href="https://kevdle.netlify.app/" target="_blank">
             <button className="bg-gray-800 text-white px-6 py-3 mt-8 rounded text-4xl hover:bg-gray-700 transition duration-300">
               Play Kevdle
             </button>
-
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
-export default GameOver
+export default GameOver;
