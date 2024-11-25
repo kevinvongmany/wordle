38f8cbe597fc2cd798cf0e6d3f9939c95e333836
Kevin Vongmany <93503441+kevinvongmany@users.noreply.github.com>
2024-11-26 10:41:16 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { FaRedo } from "react-icons/fa";
 import useWordle from "./hooks/useWordle";
 
 function App() {
-  const [targetWord, setTargetWord] = useState("HEARTH");
+  const [targetWord, setTargetWord] = useState("TOTEM");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
