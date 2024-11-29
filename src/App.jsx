eadbc80f8787d99cd6a595c24f363682a26f3c4e
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-29 21:07:06 +1100

    Updated word the of day 29 nov

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("ADCLEAR");
+  const [targetWord, setTargetWord] = useState("PULLME");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(localStorage.getItem("gameComplete") || false);
