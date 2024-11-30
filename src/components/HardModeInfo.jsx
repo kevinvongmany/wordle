9cb0063d1d953c3e2a124a571d9e719000e423f7
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-30 13:11:24 +1100

    Updated solution end screen

--- src/components/HardModeInfo.jsx
+++ src/components/HardModeInfo.jsx
@@ -4,7 +4,7 @@ const HardModeInfo = () => {
 return (
     <div className="relative cursor-pointer">
         <div className="absolute top-0 left-0 mt-12 p-3 bg-black border border-gray-300 shadow-lg w-128 md:w-256">
-            Any revealed hints/letters must be used in subsequent guesses
+            Any revealed hints (yellow or green guesses) must be used in subsequent guesses
         </div>
     </div>
 );
