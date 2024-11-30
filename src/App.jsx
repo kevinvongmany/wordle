c3e4fd4ef1646b8d229ae83024439220c151c8de
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-11-30 12:11:17 +1100

    Updated word of the day

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { FaRedo } from "react-icons/fa";
 import useWordle from "./hooks/useWordle";
 
 function App() {
-  const [targetWord, setTargetWord] = useState("RARE");
+  const [targetWord, setTargetWord] = useState("CITY");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
