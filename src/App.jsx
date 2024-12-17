724908a15d84c66509dc399fe9a15b753a745d10
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-12-17 19:28:18 +1100

    Updated bogdle of the day 17 Dec

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("VANDAL");
+  const [targetWord, setTargetWord] = useState("EPSILON");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
