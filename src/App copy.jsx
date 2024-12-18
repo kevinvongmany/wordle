94a3949af658c86d1a01339a8260768d8e201782
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-12-19 00:53:20 +1100

    Added old files for lazy reverting later

---
+++ src/App copy.jsx
@@ -0,0 +1,147 @@
+import "./App.css";
+import "./index.css";
+import { useEffect, useState } from "react";
+
+import AppContext from "./AppContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import onpamNoted from "./assets/onpamNoted.webp";
+
+import Header from "./components/Header";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import GameOver from "./components/GameOver";
+import Footer from "./components/Footer";
+import { FaRedo } from "react-icons/fa";
+
+import useWordle from "./hooks/useWordle";
+
+function App() {
+  const [targetWord, setTargetWord] = useState("LILNASX");
+  const solution = localStorage.getItem("solution");
+  const [newGame, setNewGame] = useState(solution !== targetWord);
+  const [gameComplete, setGameComplete] = useState(
+    localStorage.getItem("gameComplete") || false
+  );
+
+  const {
+    turn,
+    currentGuess,
+    guesses,
+    isCorrect,
+    maxTurns,
+    usedKeys,
+    handleKeyUp,
+    setHardMode,
+    hardMode,
+  } = useWordle(targetWord, newGame, setNewGame);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 500);
+      window.removeEventListener("keyup", handleKeyUp);
+      localStorage.setItem("gameComplete", true);
+      setGameComplete(true);
+    }
+  }, [isCorrect]);
+
+  useEffect(() => {
+    if (turn >= maxTurns) {
+      setTimeout(() => setShowModal(true), 500);
+      window.removeEventListener("keyup", handleKeyUp);
+      localStorage.setItem("gameComplete", true);
+      setGameComplete(true);
+    }
+  }, [gameComplete, turn, maxTurns]);
+
+  useEffect(() => {
+    if (!gameComplete) {
+      window.addEventListener("keyup", handleKeyUp);
+    }
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyUp]);
+
+  return (
+    <div className="flex flex-col bg-gray-900 text-white w-full">
+      <AppContext.Provider
+        value={{
+          currentGuess,
+          guesses,
+          turn,
+          isCorrect,
+          maxTurns,
+          targetWord,
+          usedKeys,
+          gameComplete,
+          handleKeyUp,
+          hardMode,
+          setHardMode,
+          setShowModal,
+        }}
+      >
+        <Header title="Wordpam"/>
+        <div className="min-h-screen px-3 py-12">
+          <div className="mt-12 flex justify-center">
+            <img
+              src={onpamNoted}
+              alt="Onpam Noted"
+              className="flex items-center mb-5 w-12 md:w-24"
+            />
+          </div>
+          <div className="flex flex-col items-center mb-4">
+            <Board />
+            <button
+              className="bg-red-800 text-white p-2 md:p-4 md:mt-4 rounded"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              <FaRedo />
+            </button>
+            <Keyboard />
+            <p className="text-center text-sm md:text-lg">
+              Want more Wordle? Try my{" "}
+              <a
+                href="https://kevdle.netlify.app/"
+                className="text-blue-400 text-underline"
+                target="_blank"
+              >
+                unthemed version
+              </a>{" "}
+              or{" "}
+              <a
+                href="https://bogdle.com/"
+                className="text-blue-400 text-underline"
+                target="_blank"
+              >
+                Bogdle
+              </a>
+              !
+            </p>
+            {showModal && <GameOver />}
+          </div>
+        </div>
+      </AppContext.Provider>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
