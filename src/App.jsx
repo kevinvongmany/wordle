31d71dff27abe80d93d4b36aeacbfb59e991cf1c
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-01-18 14:50:58 +1100

    Updated wordpam 18 jan 2024

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { FaRedo } from "react-icons/fa";
 import useWordle from "./hooks/useWordle";
 
 function App() {
-  const [targetWord, setTargetWord] = useState("XANA");
+  const [targetWord, setTargetWord] = useState("ERM");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
