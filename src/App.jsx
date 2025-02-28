cd04cdeb35fb57c624a8ceb6475f5b0988eec821
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-02-28 11:24:23 +1100

    updated word of the day for 28 feb 2025

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import useWordle from "./hooks/useWordle";
 
 function App() {
   
-  const [targetWord, setTargetWord] = useState("VIK");
+  const [targetWord, setTargetWord] = useState("GAB");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
