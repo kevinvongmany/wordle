71f90db680630f679a2bd58f8c36361728ae520a
Kevin Vongmany <93503441+kevinvongmany@users.noreply.github.com>
2025-06-05 16:22:02 +1000

    Updated word of the day

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { FaRedo } from "react-icons/fa";
 import useWordle from "./hooks/useWordle";
 
 function App() {
-  const [targetWord, setTargetWord] = useState("TRINKET");
+  const [targetWord, setTargetWord] = useState("DINNER");
   const solution = localStorage.getItem("solution");
   const [newGame, setNewGame] = useState(solution !== targetWord);
   const [gameComplete, setGameComplete] = useState(
